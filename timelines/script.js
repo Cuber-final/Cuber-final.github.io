85af4c3f5aac0601c54728cb70c233812a468d52
Cuber-final <achilescuber@gmail.com>
2022-04-20 14:12:42 +0800

    Site updated: 2022-04-20 14:12:37

---
+++ timelines/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    var cardList = document.getElementsByClassName('timeline-content')
+    var index = 0
+    for (index; index < cardList.length; index++) {
+        if (index % 2 == 1) {
+            cardList[index].classList.add('js--fadeInRight')
+        } else {
+            cardList[index].classList.add('js--fadeInLeft')
+        }
+    }
+});
+
+$(function(){
+
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+  	if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+  		$('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+  	}
+
+  	sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+  } else {
+  	
+  	sr.reveal('.js--fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+  }
+  
+  sr.reveal('.js--fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+
+});
